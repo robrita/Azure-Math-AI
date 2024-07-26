8e8ab5c1a1e92ac8655b744fad32f45179b66b67
Robert Rita <robertrita@microsoft.com>
2024-07-26 09:04:26 +0800

    added run.sh

--- app.py
+++ app.py
@@ -10,37 +10,38 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# App title
-question = app.pages.show_home()
+if __name__ == "__main__":
+    # App title
+    question = app.pages.show_home()
 
-# create a config dictionary
-config = {
-    "endpoint": os.environ["AZURE_OPENAI_ENDPOINT"],
-    "api_key": os.environ["AZURE_OPENAI_KEY"],
-    "api_version": os.environ["AZURE_OPENAI_API_VERSION"],
-    "model": os.environ["AZURE_OPENAI_CHAT_DEPLOYMENT"],
-}
+    # create a config dictionary
+    config = {
+        "endpoint": os.environ["AZURE_OPENAI_ENDPOINT"],
+        "api_key": os.environ["AZURE_OPENAI_KEY"],
+        "api_version": os.environ["AZURE_OPENAI_API_VERSION"],
+        "model": os.environ["AZURE_OPENAI_CHAT_DEPLOYMENT"],
+    }
 
-# Initialize OpenAI client with API key
-api_key = config["api_key"]
+    # Initialize OpenAI client with API key
+    api_key = config["api_key"]
 
-client = AzureOpenAI(
-    azure_endpoint=config["endpoint"],
-    api_version=config["api_version"],
-    api_key=api_key,
-)
+    client = AzureOpenAI(
+        azure_endpoint=config["endpoint"],
+        api_version=config["api_version"],
+        api_key=api_key,
+    )
 
-# app sidebar
-with st.sidebar:
-    config["model"] = app.sidebar.show_sidebar(config)
+    # app sidebar
+    with st.sidebar:
+        config["model"] = app.sidebar.show_sidebar(config)
 
-st.subheader("Answer:")
-tab1, tab2 = st.tabs(["✍️Write", "📷Upload"])
+    st.subheader("Answer:")
+    tab1, tab2 = st.tabs(["✍️Write", "📷Upload"])
 
-# type the math formula on canvas
-with tab1:
-    app.describe.show_describe(config, api_key, client, question)
+    # type the math formula on canvas
+    with tab1:
+        app.describe.show_describe(config, api_key, client, question)
 
-# Upload a picture of a math formula
-with tab2:
-    app.upload.show_upload(config, api_key, client, question)
+    # Upload a picture of a math formula
+    with tab2:
+        app.upload.show_upload(config, api_key, client, question)
